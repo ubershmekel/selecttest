09fcd4c3894c5f463eb34ebfcc079dead07d813a
Yuval Greenfield <ubershmekel@gmail.com>
2016-10-27 23:19:28 -0700

    asyncio.start_server and stream reader/writer

--- Echo/server_await.py
+++ Echo/server_await.py
@@ -1,40 +1,18 @@
-from socket import socket, SO_REUSEADDR, SOL_SOCKET
-from asyncio import Task, coroutine, get_event_loop, ensure_future
+import asyncio
 
+async def handle_echo(reader, writer):
+    print('read')
+    data = await reader.read(1024)
+    response = str(len(data)).encode('utf-8')
 
-async def handle_client(loop, client_sock):
-    print('recv')
-    buf = await loop.sock_recv(client_sock, 1024)
-    response = str(len(buf)).encode('utf-8')
-    print('send')
-    await loop.sock_sendall(client_sock, response)
-    client_sock.close()
+    print(response)
+    writer.write(response)
+    #await writer.drain()
 
-async def serve(loop):
-    server_socket = socket()
-    server_socket.bind(('localhost', 1234))
-    server_socket.listen(5)
-    tasks = []
-    while True:
-        print('accept')
-        client_sock, client_addr = await loop.sock_accept(server_socket)
-        #handle_client(loop, client_sock)
-        #ensure_future(handle_client(loop, client_sock))
-        #ensure_future(handle_client(loop, client_sock), loop=loop)
-        loop.create_task(handle_client(loop, client_sock))
-        #await loop.create_task(handle_client(loop, client_sock))
-        #peer_sock.setblocking(0)
-        #peer = Peer(self, peer_sock, peer_name)
-        #self._peers.append(peer)
-        #self.broadcast('Peer %s connected!\n' % (peer.name,))
+    print("close")
+    writer.close()
 
-def main():
-    loop = get_event_loop()
-    
-    #loop.run_until_complete(serve(loop))
-    #loop.create_task(serve(loop))
-    ensure_future(serve(loop))
-    loop.run_forever()
-
-if __name__ == '__main__':
-    main()
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, 'localhost', 1234, loop=loop)
+server = loop.run_until_complete(coro)
+loop.run_forever()
